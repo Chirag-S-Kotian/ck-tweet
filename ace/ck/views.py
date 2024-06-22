d396a744399e09d6c4d2243762ef13ae76cefb46
Chirag-S-kotian <ckotian117@gmail.com>
2024-06-22 15:27:40 +0530

    "Updated views.py: replaced get_list_or_404 with get_object_or_404, added tweet_edit function"

--- ace/ck/views.py
+++ ace/ck/views.py
@@ -2,7 +2,7 @@ import django.db
 from django.shortcuts import render
 from .models import Tweet
 from .forms import TweetForm
-from django.shortcuts import get_list_or_404,redirect
+from django.shortcuts import get_object_or_404,redirect
 
 # Create your views here.
 
@@ -24,4 +24,17 @@ def tweet_create(request):
     else:
         form = TweetForm()
     return render(request, 'tweet_form.html', {'form': form})
-      
+
+
+def tweet_edit(request,tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id , user = request.user)
+    if request.method == "POST":
+        form = TweetForm(request.POST, request.FILES, instance=tweet)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm(instance=tweet)
+    return render(request, 'tweet_form.html', {'form': form})
